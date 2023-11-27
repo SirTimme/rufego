95112ac2ae6c36afdf60e7dfc47307184d0dbc46
SirTimme <tim.piecho@gmail.com>
2023-11-27 22:16:12 +0100

    improve parsing

--- src/parser.rs
+++ src/parser.rs
@@ -4,72 +4,62 @@ peg::parser!(
     pub grammar language<'a>() for [Token<'a>] {
         use Token::*;
 
-        pub rule program() -> Program
-            = [Package] [Main] [Semicolon] decl:declaration()* [Function] [Main] [LeftParenthesis] [RightParenthesis] [LeftCurlyBrace] [RightCurlyBrace] { Program::Program }
+        pub rule program() -> Program<'a>
+            = [Package] [Main] [Semicolon] decl:declaration()* [Function] [Main] [LeftParenthesis] [RightParenthesis] [LeftCurlyBrace] [RightCurlyBrace] { Program { declarations: decl } }
 
         rule declaration() -> Declaration<'a>
-            = [Function] [LeftParenthesis] [Identifier(method_receiver_name)] [Identifier(method_receiver_type)] [RightParenthesis] [Identifier(method_name)] [LeftParenthesis] [RightParenthesis] [Identifier(return_type)] [LeftCurlyBrace] [RightCurlyBrace] { Declaration::Method }
+            = [Function] [LeftParenthesis] receiver:bound() [RightParenthesis] [Identifier(method_name)] [LeftParenthesis] params:bound()* [RightParenthesis] [Identifier(return_type)] [LeftCurlyBrace] [Return] body:expression()  [RightCurlyBrace] { Declaration::Method(receiver, method_name, params, return_type, body) }
 
-        // pub rule expr() -> Expression = precedence!{
-        //     e1:(@) [Token::Plus] e2:@ {
-        //         Expression::BinOp(Box::new(e1), BinOp::Add, Box::new(e2))
-        //     }
-        //     --
-        //     e1:(@) [Token::Star] e2:@ {
-        //         Expression::BinOp(Box::new(e1), BinOp::Mul, Box::new(e2))
-        //     }
-        //     --
-        //     [Token::Number(number)] {
-        //         Expression::Number(number)
-        //     }
-        //     [Token::LeftParenthesis] expression:expr() [Token::RightParenthesis] {
-        //         expression
-        //     }
-        // }
-        //
-        // pub rule stmt() -> Statement<'a> = precedence!{
-        //     [Var] [Identifier(name)] [Equals] expression:expr() {
-        //         Statement::Assignment(name, expression)
-        //     }
-        //     [Function] [Identifier(name)] [Token::LeftParenthesis] [Token::RightParenthesis] [Token::LeftCurlyBrace] body:([Token::Identifier(variable_name)] [Token::Identifier(variable_type)] { (variable_name, variable_type) } )* [Token::RightCurlyBrace] {
-        //         Statement::Function(name, body)
-        //     }
-        //     [Token::Package] [Token::Identifier(name)] [Token::Semicolon] {
-        //         Statement::Package(name)
-        //     }
-        // }
-        //
-        // rule type_literal() -> TypeLiteral = precedence!{
-        //     [Interface] [LeftParenthesis] body:
-        // }
-        //
-        // rule method_specification()
-        //     = []
+        rule bound() -> Binding<'a>
+            = [Identifier(variable)] [Identifier(type_)] [Comma]? { Binding { variable, type_ }}
+
+        rule expression() -> Expression<'a> = precedence!{
+            e1:(@) [Plus] e2:@ { Expression::BinOp(Box::new(e1), BinOp::Add, Box::new(e2)) }
+            --
+            e1:(@) [Star] e2:@ { Expression::BinOp(Box::new(e1), BinOp::Mul, Box::new(e2)) }
+            --
+            [Number(number)] { Expression::Number(number) }
+            [LeftParenthesis] expr:expr() [RightParenthesis] { expr }
+        }
     }
 );
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Expression {
-    Variable,
+pub struct Program<'a> {
+    declarations: Vec<Declaration<'a>>
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Declaration<'a> {
+    Type(&'a str),
+    Method(Binding<'a>, &'a str, Vec<Binding<'a>>, &'a str, Expression<'a>)
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Binding<'a> {
+    variable: &'a str,
+    type_: &'a str
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Expression<'a> {
+    Variable(&'a str),
     MethodCall,
     StructureLiteral,
     Select,
     TypeAssertion,
+    BinOp(Box<Expression<'a>>, BinOp, Box<Expression<'a>>),
+    Number(u64)
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum BinOp {
+    Add,
+    Mul,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum TypeLiteral {
     Structure,
     Interface
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Declaration<'a> {
-    Type(&'a str),
-    Method
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Program {
-    Program
 }
