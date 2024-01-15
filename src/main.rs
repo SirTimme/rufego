d323367b9eafc7ba682f876735d81b02b7010d6e
SirTimme <tim.piecho@gmail.com>
2024-01-15 16:58:49 +0100

    adjust main to call new typechecking and evaluations functions

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,30 @@ extern crate peg;
 mod token;
 mod parser;
 mod type_checker;
+mod interpreter;
 
 use std::collections::{HashMap};
 use std::fs::read_to_string;
 use logos::Logos;
 use peg::error::ParseError;
-use parser::{language, Program};
+use interpreter::{evaluate};
+use parser::{Program};
+use parser::language::program;
 use token::Token;
-use type_checker::TypeChecker;
+use type_checker::{build_type_infos, check_program};
 
 fn main() {
     if let Ok(source) = read_to_string("input/input.fgo") {
         let tokens: Result<Vec<Token>, ()> = Token::lexer(&source).collect();
-        let tokens = tokens.map_err(|_| println!("An error occurred while lexing the source")).unwrap();
+        let tokens = tokens.map_err(|_| println!("ERROR: Lexer encountered an unknown token")).unwrap();
 
-        let result: Result<Program, ParseError<usize>> = language::program(&tokens);
-        let result = result.map_err(|_| println!("An error occurred while parsing the tokens")).unwrap();
+        let result: Result<Program, ParseError<usize>> = program(&tokens);
+        let result = result.map_err(|error| println!("ERROR: {error}")).unwrap();
 
-        let mut type_checker = TypeChecker { program: &result, types: HashMap::new() };
-        type_checker.type_check();
+        let type_infos = build_type_infos(&result);
+
+        check_program(&result, &type_infos);
+
+        eprintln!("Result of program is {:?}", evaluate(&result.expression, &HashMap::new(), &type_infos));
     }
 }
