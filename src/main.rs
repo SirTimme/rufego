cecafa41eb9609ceb3aaa0039b4036f0c9b8ffaf
SirTimme <tim.piecho@gmail.com>
2024-01-16 13:05:52 +0100

    refactored main to use match for source file reading

--- src/main.rs
+++ src/main.rs
@@ -15,44 +15,49 @@ use token::{LexerError, Token};
 use type_checker::{build_type_infos, check_program};
 
 fn main() {
-    if let Ok(source) = read_to_string("input/input.fgo") {
-        let token_result: Result<Vec<Token>, LexerError> = Token::lexer(&source).collect();
+    match read_to_string("input/input.fgo") {
+        Ok(source) => {
+            let token_result: Result<Vec<Token>, LexerError> = Token::lexer(&source).collect();
 
-        match token_result {
-            Ok(tokens) => {
-                match parse(&tokens) {
-                    Ok(program) => {
-                        match build_type_infos(&program) {
-                            Ok(type_infos) => {
-                                match check_program(&program, &type_infos) {
-                                    Ok(_) => {
-                                        match evaluate(&program.expression, &HashMap::new(), &type_infos) {
-                                            Ok(value) => {
-                                                eprintln!("Program evaluates to {:?}", value);
-                                            }
-                                            Err(eval_error) => {
-                                                eprintln!("Evaluation of program failed with following error: {:?}", eval_error.message);
+            match token_result {
+                Ok(tokens) => {
+                    match parse(&tokens) {
+                        Ok(program) => {
+                            match build_type_infos(&program) {
+                                Ok(type_infos) => {
+                                    match check_program(&program, &type_infos) {
+                                        Ok(_) => {
+                                            match evaluate(&program.expression, &HashMap::new(), &type_infos) {
+                                                Ok(value) => {
+                                                    eprintln!("Program evaluates to {:?}", value);
+                                                }
+                                                Err(eval_error) => {
+                                                    eprintln!("Evaluation of program failed with following error: {:?}", eval_error.message);
+                                                }
                                             }
                                         }
-                                    }
-                                    Err(type_error) => {
-                                        eprintln!("Typechecking failed with the following error: {:?}", type_error.message);
+                                        Err(type_error) => {
+                                            eprintln!("Typechecking failed with the following error: {:?}", type_error.message);
+                                        }
                                     }
                                 }
-                            }
-                            Err(type_info_error) => {
-                                eprintln!("Creating the type infos failed with the following error: {:?}", type_info_error.message);
+                                Err(type_info_error) => {
+                                    eprintln!("Creating the type infos failed with the following error: {:?}", type_info_error.message);
+                                }
                             }
                         }
-                    }
-                    Err(parser_error) => {
-                        eprintln!("Parsing failed with the following error: {parser_error}")
+                        Err(parser_error) => {
+                            eprintln!("Parsing failed with the following error: {parser_error}")
+                        }
                     }
                 }
+                Err(lexer_error) => {
+                    eprintln!("Lexing the source failed with the following error: {:?}", lexer_error)
+                }
             }
-            Err(lexer_error) => {
-                eprintln!("Lexing the source failed with the following error: {:?}", lexer_error)
-            }
+        }
+        Err(error) => {
+            eprintln!("Reading the source file failed with the following error: {:?}", error)
         }
     }
 }
